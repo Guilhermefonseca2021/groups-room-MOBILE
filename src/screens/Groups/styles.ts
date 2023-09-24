b1ab1f6f240b87be9d240cfb2c9023b7305e9eda
guilherme.fonseca2021 <gsbloogs194@gmail.com>
2023-09-24 20:58:23 -0300

    library and initial setup

---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,15 @@
+//  '/native' para indicar q é para o mobile
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    flex: 1;
+    background-color: #000;
+    color: green;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Title = styled.Text`
+    color: #fff;
+    font-size: 32;
+`;
