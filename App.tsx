caadafe16580df4bb827c829c6fda4b19e4f006f
guilherme.fonseca2021 <gsbloogs194@gmail.com>
2023-09-25 11:36:57 -0300

    setup inicial

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
 import { Groups } from '@screens/Groups';
 
+import { ThemeProvider } from 'styled-components';
+import theme from './src/theme/theme'
+
+
 export default function App() {
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
   );
 }
